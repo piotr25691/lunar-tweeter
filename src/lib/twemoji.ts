8ac488da80efbeff1cfd361a1cba5aab61a7c2a1
Piotr Zalewski <bardzoglodnypiotrek@gmail.com>
2024-09-22 06:18:13 +0000

    feat: sanitize twemoji

--- src/lib/twemoji.ts
+++ src/lib/twemoji.ts
@@ -1,3 +1,5 @@
+import DOMPurify from 'dompurify';
+
 export function twemojiParse(input: string): string {
   const emojiRegex =
     /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F1E6}-\u{1F1FF}\u{1F900}-\u{1F9FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/u;
@@ -17,5 +19,5 @@ export function twemojiParse(input: string): string {
     } else result += char;
   });
 
-  return result;
+  return DOMPurify.sanitize(result);
 }
