1f73b1e7dcef538d59658afa906b17979c70d647
Piotr Zalewski <bardzoglodnypiotrek@gmail.com>
2024-09-25 15:19:33 +0000

    NO WORKY

--- src/lib/twemoji.ts
+++ src/lib/twemoji.ts
@@ -1,5 +1,6 @@
 import DOMPurify from 'dompurify';
 
+// bruh
 export function twemojiParse(input: string): string {
   const emojiRegex =
     /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F1E6}-\u{1F1FF}\u{1F900}-\u{1F9FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/u;
